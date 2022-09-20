93f5a0dbef390ab6f3226878f87d41a76d44f9dc
nicoelias997 <nicoelias997@gmail.com>
2022-09-20 14:04:07 +0200

    Agrego condicionales para que aparezan y desaparezcan botones, con user activo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector} from 'react-redux'
 import {Link, NavLink, useNavigate} from 'react-router-dom'
 import { cerraSesionAccion } from '../redux/usuarioDucks'
 
@@ -13,30 +13,38 @@ const Navbar = () => {
         dispatch(cerraSesionAccion())
         navigate("/login")
     }
+    const activo = useSelector(store => store.usuario.activo)
 
     return (
         <div className="navbar navbar-dark bg-dark">
             <Link to="/" className="navbar-brand">Poke API</Link>
             <div>
                 <div className="d-flex">
-                    <NavLink 
-                        className="btn btn-dark mr-2" 
-                        to="/"
-                    >
-                        Inicio
-                    </NavLink>
-                    <NavLink 
-                        className="btn btn-dark mr-2" 
-                        to="/login"
-                    >
-                        Login
-                    </NavLink>
-                    <button
-                        className="btn btn-dark"
-                        onClick={() => cerrarSesion()}
-                    >
-                        cerrar Sesión
-                    </button>
+                    {
+                        activo ? (
+                            <>
+                                <NavLink 
+                                    className="btn btn-dark mr-2" 
+                                    to="/"
+                                >
+                                    Pokemon
+                                </NavLink>
+                                <button
+                                    className="btn btn-dark"
+                                    onClick={() => cerrarSesion()}
+                                >
+                                    cerrar Sesión
+                                </button>
+                            </>
+                        ) : (
+                            <NavLink 
+                                className="btn btn-dark mr-2" 
+                                to="/login"
+                            >
+                                Login
+                            </NavLink>
+                        )
+                    }
                 </div>
             </div>
         </div>
